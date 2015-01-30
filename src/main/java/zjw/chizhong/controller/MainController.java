e4e7a78361474a1ee7e722fa118aa3e4708a08dc
zjwben <348906525@qq.com>
2015-01-30 18:05:17 +0800

    init rebuild project it can run html page successfully

---
+++ src/main/java/zjw/chizhong/controller/MainController.java
@@ -0,0 +1,16 @@
+package zjw.chizhong.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value="/index",method=RequestMethod.GET)
+	public ModelAndView test(){
+		return new ModelAndView("index");
+	}
+}
